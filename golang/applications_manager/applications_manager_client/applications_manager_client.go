3339e6e23f394103b02e6320fcc835784b3db305
davecourtois <dave.courtois60@gmail.com>
2021-08-26 21:25:22 -0400

    ...

--- golang/applications_manager/applications_manager_client/applications_manager_client.go
+++ golang/applications_manager/applications_manager_client/applications_manager_client.go
@@ -350,6 +350,7 @@ func (client *Applications_Manager_Client) DeployApplication(user string, name s
 		iconPath := strings.ReplaceAll(absolutePath, "\\", "/")
 		lastIndex := strings.LastIndex(iconPath, "/")
 		iconPath = iconPath[0:lastIndex] + "/" + packageConfig["icon"].(string)
+		log.Println("--------> icon path ", iconPath)
 		if Utility.Exists(iconPath) {
 			// Convert to png before creating the data url.
 			if strings.HasSuffix(strings.ToLower(iconPath), ".svg") {
