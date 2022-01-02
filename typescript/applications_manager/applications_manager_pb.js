90eb52df2d0e2ab5e647d4a8cb6cdb538ef1fe2d
davecourtois <dave.courtois60@gmail.com>
2022-01-01 23:01:24 -0500

    ...

--- typescript/applications_manager/applications_manager_pb.js
+++ typescript/applications_manager/applications_manager_pb.js
@@ -11,7 +11,7 @@ var jspb = require('google-protobuf');
 var goog = jspb;
 var global = Function('return this')();
 
-var resource_pb = require('../resource/resource_pb.js');
+var resource_pb = require('./resource_pb.js');
 goog.exportSymbol('proto.applications_manager.DeployApplicationRequest', null, global);
 goog.exportSymbol('proto.applications_manager.DeployApplicationResponse', null, global);
 goog.exportSymbol('proto.applications_manager.InstallApplicationRequest', null, global);
