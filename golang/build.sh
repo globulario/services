9c660368ea73200a60a71ee4596e83ac37c944ed
davecourtois <dave.courtois60@gmail.com>
2022-10-28 12:03:37 -0400

    ...

--- golang/build.sh
+++ golang/build.sh
@@ -3,37 +3,38 @@
 # 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
 #There is the command to build all services at once.
 # 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
-go build -o ./admin/admin_server ./admin/admin_server
-go build -o ./applications_manager/applications_manager_server ./applications_manager/applications_manager_server
-go build -o ./services_manager/services_manager_server ./services_manager/services_manager_server
-go build -o ./authentication/authentication_server ./authentication/authentication_server
-go build -o ./catalog/catalog_server ./catalog/catalog_server
-go build -o ./blog/blog_server ./blog/blog_server
-go build -o ./conversation/conversation_server ./conversation/conversation_server
-go build -o ./discovery/discovery_server ./discovery/discovery_server
-go build -o ./dns/dns_server ./dns/dns_server
-go build -o ./echo/echo_server ./echo/echo_server
-go build -o ./event/event_server ./event/event_server
-go build -o ./file/file_server ./file/file_server
-go build -o ./ldap/ldap_server ./ldap/ldap_server
-go build -o ./log/log_server ./log/log_server
-go build -o ./mail/mail_server ./mail/mail_server
-go build -o ./monitoring/monitoring_server ./monitoring/monitoring_server
-go build -o ./persistence/persistence_server ./persistence/persistence_server
-go build -o ./rbac/rbac_server ./rbac/rbac_server
-go build -o ./repository/repository_server ./repository/repository_server
-go build -o ./resource/resource_server ./resource/resource_server
-go build -o ./search/search_server ./search/search_server
-go build -o ./sql/sql_server ./sql/sql_server
-go build -o ./storage/storage_server ./storage/storage_server
-go build -o ./config/config_server ./config/config_server
-go build -o ./title/title_server ./title/title_server
-go build -o ./torrent/torrent_server ./torrent/torrent_server
+go build  -buildvcs=false -o ./admin/admin_server ./admin/admin_server
+go build  -buildvcs=false -o ./applications_manager/applications_manager_server ./applications_manager/applications_manager_server
+go build  -buildvcs=false -o ./services_manager/services_manager_server ./services_manager/services_manager_server
+go build  -buildvcs=false -o ./authentication/authentication_server ./authentication/authentication_server
+go build  -buildvcs=false -o ./catalog/catalog_server ./catalog/catalog_server
+go build  -buildvcs=false -o ./blog/blog_server ./blog/blog_server
+go build  -buildvcs=false -o ./conversation/conversation_server ./conversation/conversation_server
+go build  -buildvcs=false -o ./discovery/discovery_server ./discovery/discovery_server
+go build  -buildvcs=false -o ./dns/dns_server ./dns/dns_server
+go build  -buildvcs=false -o ./echo/echo_server ./echo/echo_server
+go build  -buildvcs=false -o ./event/event_server ./event/event_server
+go build  -buildvcs=false -o ./file/file_server ./file/file_server
+go build  -buildvcs=false -o ./ldap/ldap_server ./ldap/ldap_server
+go build  -buildvcs=false -o ./log/log_server ./log/log_server
+go build  -buildvcs=false -o ./mail/mail_server ./mail/mail_server
+go build  -buildvcs=false -o ./monitoring/monitoring_server ./monitoring/monitoring_server
+go build  -buildvcs=false -o ./persistence/persistence_server ./persistence/persistence_server
+go build  -buildvcs=false -o ./rbac/rbac_server ./rbac/rbac_server
+go build  -buildvcs=false -o ./repository/repository_server ./repository/repository_server
+go build  -buildvcs=false -o ./resource/resource_server ./resource/resource_server
+go build  -buildvcs=false -o ./search/search_server ./search/search_server
+go build  -buildvcs=false -o ./sql/sql_server ./sql/sql_server
+go build  -buildvcs=false -o ./storage/storage_server ./storage/storage_server
+go build  -buildvcs=false -o ./config/config_server ./config/config_server
+go build  -buildvcs=false -o ./title/title_server ./title/title_server
+go build  -buildvcs=false -o ./torrent/torrent_server ./torrent/torrent_server
 
 # start services...
 export GLOBULAR_SERVICES_ROOT=/home/dave/globulario/services
 
 ./admin/admin_server/admin_server &
+./blog/blog_server/blog_server &
 ./applications_manager/applications_manager_server/applications_manager_server &
 ./services_manager/services_manager_server/services_manager_server &
 ./authentication/authentication_server/authentication_server &
