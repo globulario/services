8c9289fae652d74dee41d26c8746f5cf2a619ad5
davecourtois <dave.courtois60@gmail.com>
2023-01-10 11:31:53 -0500

    ...

--- golang/echo/echo_client/echo_test.go
+++ golang/echo/echo_client/echo_test.go
@@ -12,8 +12,9 @@ import (
 func TestEcho(t *testing.T) {
 
 	// Connect to the plc client.
-	// "0f80ed1a-5d3a-46f1-a3f6-c091ac259665",  "b94d0011-39a0-4bdb-9a5c-7e9abc23b26b"
+	
 	client, err := NewEchoService_Client("globule-mac-mini.globular.cloud:10602", "echo.EchoService")
+	//client, err := NewEchoService_Client("globule-dell.globular.cloud", "echo.EchoService")
 	if err != nil {
 		log.Println("17 ---> ", err)
 		return
